3b15f77bfb6dd6f8e451b97a537f5f478f0ecdbc
mc-solo <revatoel@gmail.com>
2025-12-24 11:14:42 +0300

    create: adds db.go

---
+++ services/customer/internal/db/db.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/mc-solo/subscription-billing-sys/services/customer/internal/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Init() *gorm.DB {
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		log.Fatal("DSN env variable not set")
+	}
+
+	// config GORM to use a custom loger to get the sql queries in dev
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to db")
+	}
+
+	// todo: replace with golang-migrate [for now i'll just use automigrate from gorm]
+
+	log.Println("Migrating database schema...")
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	return db
+}
